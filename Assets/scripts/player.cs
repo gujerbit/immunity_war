42bc9f35ddc5cc8e0115afb18987bf3ebf50769d
이준영 <junyeong@aircuve.com>
2022-06-15 15:51:11 +0900

    - script utf-8 설정

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    Rigidbody2D rigidBody;
+
+    private void Reset() // first life cycle
+    {
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate() // 일정 주기마다 계속 실행됨 (Default: 0.02s)
+    {
+        
+    }
+
+    void Update() // 매 프레임마다 호출됨 (주기가 일정치 않음)
+    {
+        
+    }
+}
