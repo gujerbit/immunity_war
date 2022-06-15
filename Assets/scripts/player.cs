64330cf4d87f2615580f6d6ff1c836322b3105d0
gujerbit <ghftm1124@gmail.com>
2022-06-15 17:09:29 +0900

    플레이어 이동 로직 추가

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -1,29 +1,33 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    Rigidbody2D rigidBody;
+    public float moveSpeed = 5f;
 
-    private void Reset() // first life cycle
+    private Rigidbody2D rigidBody;
+
+    private void Awake() // 스크립트가 실행될 때 한 번만 호출됨, 두번째로 호출됨
     {
         rigidBody = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    private void FixedUpdate() // 일정 주기마다 계속 실행됨 (Default: 0.02s)
+    private void FixedUpdate() // 일정 주기마다 계속 호출됨 (Default 0.02s)
     {
-        
+        float moveX = Input.GetAxis("Horizontal"); // x축 힘 (-1f ~ 1f)
+        float moveY = Input.GetAxis("Vertical"); // y축 힘 (-1f ~ 1f)
+
+        rigidBody.velocity = new Vector2(moveX, moveY) * moveSpeed;
     }
 
     void Update() // 매 프레임마다 호출됨 (주기가 일정치 않음)
     {
-        
+
     }
 }
